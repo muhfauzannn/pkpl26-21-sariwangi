12bab1d0bd301fbc0c9fa6c523840e777c70c852
muhfauzannn <fauzannmuhh@gmail.com>
2026-05-07 23:24:49 +0700

    feat: add authentication URLs to the main URL configuration

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/", include("apps.authentication.urls")),
 ]
 
 if settings.DEBUG:

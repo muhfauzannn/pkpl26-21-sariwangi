4d3987210395aa30513ad18454b1832f5dc7c100
Saikhah Ummu <saikhah.ummu@ui.ac.id>
2026-05-08 10:36:05 +0700

    feat: enhance candidate registration and detail pages with improved layout and new features

--- config/settings/base.py
+++ config/settings/base.py
@@ -109,6 +109,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
